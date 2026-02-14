ef50e5973d4827968e2f14f43067d0d1d021c516
buildquoteau-jpg <buildquoteau@gmail.com>
2026-02-14 23:47:23 +0800

    Add Manufacturer Dashboard Screen component

---
+++ src/app/manufacturer/ManufacturerDashboardScreen.tsx
@@ -0,0 +1,13 @@
+// Manufacturer Dashboard Screen component code here
+
+import React from 'react';
+
+const ManufacturerDashboardScreen = () => {
+    return (
+        <div>
+            <h1>Manufacturer Dashboard</h1>
+        </div>
+    );
+};
+
+export default ManufacturerDashboardScreen;
